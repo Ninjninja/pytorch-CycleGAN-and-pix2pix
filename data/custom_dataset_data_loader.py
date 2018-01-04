62259f10222575b68a0eb9bbc566c48424341c73
Niranjan <kniranjankumar.eee@gmail.com>
2018-01-04 16:38:19 +0530

    new files for block push dataset

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -10,7 +10,7 @@ def CreateDataset(opt):
     elif opt.dataset_mode == 'unaligned':
         from data.unaligned_dataset import UnalignedDataset
         dataset = UnalignedDataset()
-    elif opt.dataset_mode == 'MultiAlignedDataset':
+    elif opt.dataset_mode == 'Multi':
         from data.multi_image_dataset import multi_image_dataset
         dataset = multi_image_dataset()
     elif opt.dataset_mode == 'single':
